3f7da26f1266d0ed34ed42912b4996453184cd71
전지훈 <junji636@gmail.com>
2023-01-26 23:56:54 +0900

    first Commit

---
+++ SwiftUI_Popover/SwiftUI_Popover/SwiftUI_PopoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PopoverApp.swift
+//  SwiftUI_Popover
+//
+//  Created by 전지훈 on 2023/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PopoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
